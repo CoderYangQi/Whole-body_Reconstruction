108902d1d83466cdb33648d1c2fae9f6b0195059
yqustc <1055395921@qq.com>
2026-08-18 13:59:40 +0800

    DUST upload batch 5 of 13

---
+++ TransMorph_MultiScale/data/datasets.py
@@ -0,0 +1,202 @@
+import os, glob
+import torch, sys
+from torch.utils.data import Dataset
+from .data_utils import pkload
+import matplotlib.pyplot as plt
+
+import numpy as np
+import SimpleITK as sitk
+class VISoRDataset(Dataset):
+    def __init__(self, fixed_data_path, moving_data_path,transforms):
+        self.fixed_data_path = fixed_data_path
+        self.moving_data_path = moving_data_path
+        self.transforms = transforms
+
+    def one_hot(self, img, C):
+        out = np.zeros((C, img.shape[1], img.shape[2], img.shape[3]))
+        for i in range(C):
+            out[i,...] = img == i
+        return out
+
+    def PreprocessVISoRData(self,img):
+        # img = sitk.Clamp((sitk.Log(sitk.Cast(img, sitk.sitkFloat32)) - 4.6) * 39.4,
+        #                    sitk.sitkFloat32, 0, 255)/255
+        img = img / 255.0
+
+        return img
+
+    def __getitem__(self, index):
+        # path = self.fixed_data_path[index]
+        x = sitk.ReadImage(self.fixed_data_path[index])[:,:,8:72]
+        y = sitk.ReadImage(self.moving_data_path[index])[:,:,8:72]
+        # trans 0~65536 to 0~1
+        x = self.PreprocessVISoRData(x); y = self.PreprocessVISoRData(y)
+        x = sitk.GetArrayFromImage(x); y = sitk.GetArrayFromImage(y)
+
+
+        # y = np.flip(y, 1)
+        x, y = x[None, ...], y[None, ...]
+        x,y = self.transforms([x, y])
+        x = np.ascontiguousarray(x)
+        x = torch.from_numpy(x)
+        y = np.ascontiguousarray(y)
+        y = torch.from_numpy(y)
+        return x, y
+
+    def __len__(self):
+        return len(self.fixed_data_path)
+class VISoRSegDataset(Dataset):
+    def __init__(self, fixed_data_path, moving_data_path, fixed_label_path, moving_label_path,transforms):
+        self.fixed_data_path = fixed_data_path
+        self.moving_data_path = moving_data_path
+        self.transforms = transforms
+        self.fixed_label_path = fixed_label_path
+        self.moving_label_path = moving_label_path
+
+    def one_hot(self, img, C):
+        out = np.zeros((C, img.shape[1], img.shape[2], img.shape[3]))
+        for i in range(C):
+            out[i,...] = img == i
+        return out
+
+    def PreprocessVISoRData(self,img):
+        # img = sitk.Clamp((sitk.Log(sitk.Cast(img, sitk.sitkFloat32)) - 4.6) * 39.4,
+        #                    sitk.sitkFloat32, 0, 255)/255
+        img = img / 255.0
+
+        return img
+
+    def __getitem__(self, index):
+        # path = self.fixed_data_path[index]
+        x = sitk.ReadImage(self.fixed_data_path[index])
+        y = sitk.ReadImage(self.moving_data_path[index])
+
+        x_size = x.GetSize()
+        if x_size[2] > 64:
+            x = x[:,:, 8: 72]
+            y = y[:,:, 8: 72]
+        # trans 0~65536 to 0~1
+        x = self.PreprocessVISoRData(x); y = self.PreprocessVISoRData(y)
+        x = sitk.GetArrayFromImage(x); y = sitk.GetArrayFromImage(y)
+
+        x_seg = sitk.ReadImage(self.fixed_label_path[index])[:,:,8:72]
+        y_seg = sitk.ReadImage(self.moving_label_path[index])[:,:,8:72]
+        x_seg = sitk.GetArrayFromImage(x_seg); y_seg = sitk.GetArrayFromImage(y_seg)
+
+
+        # y = np.flip(y, 1)
+        x, y = x[None, ...], y[None, ...]
+        x_seg, y_seg = x_seg[None, ...], y_seg[None, ...]
+
+        x, x_seg = self.transforms([x, x_seg])
+        y, y_seg = self.transforms([y, y_seg])
+        # y = self.one_hot(y, 2)
+        # print(y.shape)
+        # sys.exit(0)
+        x = np.ascontiguousarray(x)  # [Bsize,channelsHeight,,Width,Depth]
+        y = np.ascontiguousarray(y)
+        x_seg = np.ascontiguousarray(x_seg)  # [Bsize,channelsHeight,,Width,Depth]
+        y_seg = np.ascontiguousarray(y_seg)
+        # plt.figure()
+        # plt.subplot(1, 2, 1)
+        # plt.imshow(x[0, :, :, 8], cmap='gray')
+        # plt.subplot(1, 2, 2)
+        # plt.imshow(y[0, :, :, 8], cmap='gray')
+        # plt.show()
+        # sys.exit(0)
+        # y = np.squeeze(y, axis=0)
+        x, y, x_seg, y_seg = torch.from_numpy(x), torch.from_numpy(y), torch.from_numpy(x_seg), torch.from_numpy(y_seg)
+        return x, y, x_seg, y_seg
+
+    def __len__(self):
+        return len(self.fixed_data_path)
+
+
+
+
+class JHUBrainDataset(Dataset):
+    def __init__(self, data_path, transforms):
+        self.paths = data_path
+        self.transforms = transforms
+
+    def one_hot(self, img, C):
+        out = np.zeros((C, img.shape[1], img.shape[2], img.shape[3]))
+        for i in range(C):
+            out[i,...] = img == i
+        return out
+
+    def __getitem__(self, index):
+        path = self.paths[index]
+        x, y = pkload(path)
+        #print(x.shape)
+        #print(x.shape)
+        #print(np.unique(y))
+        # print(x.shape, y.shape)#(240, 240, 155) (240, 240, 155)
+        # transforms work with nhwtc
+        x, y = x[None, ...], y[None, ...]
+        # print(x.shape, y.shape)#(1, 240, 240, 155) (1, 240, 240, 155)
+        x,y = self.transforms([x, y])
+        #y = self.one_hot(y, 2)
+        #print(y.shape)
+        #sys.exit(0)
+        x = np.ascontiguousarray(x)# [Bsize,channelsHeight,,Width,Depth]
+        y = np.ascontiguousarray(y)
+        #plt.figure()
+        #plt.subplot(1, 2, 1)
+        #plt.imshow(x[0, :, :, 8], cmap='gray')
+        #plt.subplot(1, 2, 2)
+        #plt.imshow(y[0, :, :, 8], cmap='gray')
+        #plt.show()
+        #sys.exit(0)
+        #y = np.squeeze(y, axis=0)
+        x, y = torch.from_numpy(x), torch.from_numpy(y)
+        return x, y
+
+    def __len__(self):
+        return len(self.paths)
+
+
+class JHUBrainInferDataset(Dataset):
+    def __init__(self, data_path, transforms):
+        self.paths = data_path
+        self.transforms = transforms
+
+    def one_hot(self, img, C):
+        out = np.zeros((C, img.shape[1], img.shape[2], img.shape[3]))
+        for i in range(C):
+            out[i,...] = img == i
+        return out
+
+    def __getitem__(self, index):
+        path = self.paths[index]
+        x, y, x_seg, y_seg = pkload(path)
+        #print(x.shape)
+        #print(x.shape)
+        #print(np.unique(y))
+        # print(x.shape, y.shape)#(240, 240, 155) (240, 240, 155)
+        # transforms work with nhwtc
+        x, y = x[None, ...], y[None, ...]
+        x_seg, y_seg= x_seg[None, ...], y_seg[None, ...]
+        # print(x.shape, y.shape)#(1, 240, 240, 155) (1, 240, 240, 155)
+        x, x_seg = self.transforms([x, x_seg])
+        y, y_seg = self.transforms([y, y_seg])
+        #y = self.one_hot(y, 2)
+        #print(y.shape)
+        #sys.exit(0)
+        x = np.ascontiguousarray(x)# [Bsize,channelsHeight,,Width,Depth]
+        y = np.ascontiguousarray(y)
+        x_seg = np.ascontiguousarray(x_seg)  # [Bsize,channelsHeight,,Width,Depth]
+        y_seg = np.ascontiguousarray(y_seg)
+        #plt.figure()
+        #plt.subplot(1, 2, 1)
+        #plt.imshow(x[0, :, :, 8], cmap='gray')
+        #plt.subplot(1, 2, 2)
+        #plt.imshow(y[0, :, :, 8], cmap='gray')
+        #plt.show()
+        #sys.exit(0)
+        #y = np.squeeze(y, axis=0)
+        x, y, x_seg, y_seg = torch.from_numpy(x), torch.from_numpy(y), torch.from_numpy(x_seg), torch.from_numpy(y_seg)
+        return x, y, x_seg, y_seg
+
+    def __len__(self):
+        return len(self.paths)
